ab187f5a8b15a29c015285bb2092907c9965fdf7
Brigette LeVert <bnl.levert@gmail.com>
2020-02-03 00:19:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,7 @@ class Song
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
     new_song = self.new(song)
-    new_song.artist_name = artist
+    new_song.artist = artist
     new_song
   end
 

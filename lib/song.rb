3fef42ff235d044339305dd6ee934558a363c095
Brigette LeVert <bnl.levert@gmail.com>
2020-02-07 15:09:01 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :artist, :name
 
@@ -9,29 +10,22 @@ class Song
   end
 
   def artist_name=(name)
+    @name = name
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+    Artist.add_song(self)
   end
 
-  # def self.new_by_filename(file)
-  #   song_info = file.chomp(".mp3").split(" - ")
-  #   song = Song.new(song_info[1])
-  #   song.artist_name = song_info[0]
-  #   song
-  # end
-  
+ 
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
+  #  binding.pry 
     artist = file_name.split(" - ")[0]
+    #binding.pry 
     new_song = self.new(song)
-    new_song.artist = artist
+    new_song.artist = Artist.new(artist)
     new_song
+   # binding.pry 
   end
-
-  # def artist_name=(name)
-  #   self.artist = Artist.find_or_create_by_name(name)
-  #   artist.add_song(self)
-  # end
 
   
   def self.all 
